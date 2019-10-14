e2bb883f49d9cc0dd5e9ef9f9d70929b74ff79c3
Daniel Pruitt <danielpruitt6@gmail.com>
2019-10-14 15:44:35 -0400

    follow the neon logo, cant change color

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './Header.css';
+import MyParticles from '../Particles/Particles';
 
 const Header = () => (
-    <div> 
-        <img src={require('../../Images/FollowTheNeon_Sign_Outrun.jpg')} alt='follow the neon' className='cust-img mx-auto d-block'/>
+    <div>
+        <MyParticles/>
+        <img src={require('../../Images/FollowTheNeon_SignWithGap_B&W_Outrun.png')} alt='follow the neon' className='png-effect cust-img mx-auto d-block' />
+
+        {/* <img src={require('../../Images/FollowTheNeon_Sign_Outrun.jpg')} alt='follow the neon' className='cust-img mx-auto d-block'/> */}
     </div>
 )
 
